d8fb441bafd1d716000c6f705919f471d3a51847
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-26 18:55:10 +0100

    added guards file then deleted them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
+// GUARDS
+
+
 const routes: Routes = [
   {path: '', component: LandingPageComponent}
 ];
+
+
 
 @NgModule({
   declarations: [

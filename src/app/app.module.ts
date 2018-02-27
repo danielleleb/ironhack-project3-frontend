f21dba1cc0fbd592ce59c61e5c06bc70c4513f40
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-27 12:43:48 +0100

    added login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-import { AppComponent } from './app.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 // GUARDS
 
+import { AuthService } from './services/auth.service';
+import { RequireAnonGuardService } from './guards/require-anon-guard.service';
+import { RequireUserGuardService } from './guards/require-user-guard.service';
+import { InitAuthGuardService } from './guards/init-auth-guard.service';
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent}
+  { path: '',  component: LandingPageComponent
+  , canActivate: [ InitAuthGuardService ]
+ },
+  { path: 'login',  component: LoginPageComponent
+  , canActivate: [ RequireAnonGuardService ]
+ },
+  { path: 'signup',  component: SignupPageComponent
+  , canActivate: [ RequireAnonGuardService ] 
+},
+  { path: 'page',  component: HomePageComponent
+   , canActivate: [ RequireUserGuardService ] 
+  },
+  { path: '**', redirectTo: '' }
 ];
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    InitAuthGuardService,
+    RequireAnonGuardService,
+    RequireUserGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-signup-page',
+  templateUrl: './signup-page.component.html',
+  styleUrls: ['./signup-page.component.css']
+})
+export class SignupPageComponent implements OnInit {
+
+  feedbackEnabled = false;
+  error = null;
+  processing = false;
+  username: String;
+  password: String
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  submitForm(form) {
+    this.error = '';
+    this.feedbackEnabled = true;
+    if (form.valid){
+    this.processing = true;
+    const data = {
+      username: this.username,
+      password: this.password
+    }
+      this.authService.signup(data)
+        .then((result) => {
+            this.router.navigate(['/homepage'])
+      //     // ... navigate with this.router.navigate(['...'])
+        })
+        .catch((err) => {
+          this.error = err.error.error; // :-)
+          this.processing = false;
+          this.feedbackEnabled = false;
+        });
+  }
+}
+}

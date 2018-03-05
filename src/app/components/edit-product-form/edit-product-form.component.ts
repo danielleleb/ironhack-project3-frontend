574ea79af548b612e7439025ccc272a6d03bd01c
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-05 18:05:23 +0100

    added form to form component

--- src/app/components/edit-product-form/edit-product-form.component.ts
+++ src/app/components/edit-product-form/edit-product-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-edit-product-form',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-product-form.component.scss']
 })
 export class EditProductFormComponent implements OnInit {
+
+  @Input() product
 
   constructor() { }
 

0fc576000c20918f726f10be25d96afeb4b11791
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-05 19:19:35 +0100

    all products are being retrieved on edit page

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/toPromise';
 import { environment } from '../../environments/environment';
 
@@ -8,27 +11,44 @@ import { environment } from '../../environments/environment';
 @Injectable()
 export class ProductsService {
 
+  private product: any;
+  private productChange: Subject<any> = new Subject();
+
   private API_URL =  environment.apiUrl + '/products';
 
+
+
   constructor(private httpClient: HttpClient) { }
+
+  private setProduct(product?: any) {
+    this.product = product;
+    this.productChange.next(product);
+    return product;
+  }
 
   addNewProduct(product: any): Promise<any> {
     const options = {
       withCredentials: true
     };
     return this.httpClient.post(`${this.API_URL}/add-product`, product, options)
-      .toPromise();
+      .toPromise()
+      .then((data) => this.setProduct(data));
   }
   getProductList(businessId) :Promise<any>{
     const options = {
       withCredentials: true
     };
     return this.httpClient.get(`${this.API_URL}/${businessId}`, options)
-     .toPromise()
-      .then((data) => {
-        console.log('getProductList', data);
-        return data;
-      })
+    .toPromise()
+    .then((data) => this.setProduct(data))
+  }
+  getEntireProductList(businessId) :Promise<any>{
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.get(`${this.API_URL}/complete/${businessId}`, options)
+    .toPromise()
+    // .then((data) => this.setProduct(data))
   }
   
   getProductById(productId) {
@@ -36,23 +56,17 @@ export class ProductsService {
       withCredentials: true
     };
     return this.httpClient.get(`${this.API_URL}/view/${productId}`, options)
-     .toPromise()
-      .then((data) => {
-        console.log('getProductById', data);
-        return data;
-      })
+    .toPromise()
+    .then((data) => this.setProduct(data))
   }
 
-  getProductsBySearch(citySearch) :Promise<any>{
+  getProductsBySearch(citySearch, typeSearch) :Promise<any>{
     const options = {
       withCredentials: true
     };
-    return this.httpClient.get(`${this.API_URL}/search?terms=${citySearch}`, options)
-     .toPromise()
-      .then((data) => {
-        console.log('getProductsByS', data);
-        return data;
-      })
+    return this.httpClient.get(`${this.API_URL}/view/${citySearch}/${typeSearch}`, options)
+    .toPromise()
+    .then((data) => this.setProduct(data))
   }
 
   bookProduct(product: any){
@@ -60,11 +74,17 @@ export class ProductsService {
       withCredentials: true
     };
     return this.httpClient.post(`${this.API_URL}/book`, product, options)
-     .toPromise()
-      .then((data) => {
-        console.log('pr', data);
-        return data;
-      })
+    .toPromise()
+    .then((data) => this.setProduct(data))
+  }
+
+  returnProduct(productId) {
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.post(`${this.API_URL}/return/${productId}`, options)
+    .toPromise()
+    .then((data) => this.setProduct(data))
   }
 
 }

65e5f77c90aa133728293b9db050a8f240f22a36
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-07 19:28:31 +0100

    added login on popup when you try to rent without logging in

--- src/app/pages/business-profile/business-profile.component.ts
+++ src/app/pages/business-profile/business-profile.component.ts
@@ -83,6 +83,26 @@ if (this.user._id == this.businessId) {
   }
   
   }
+
+  handleLoginForm(event) {
+    this.error = null;
+      this.authService.login(event)
+        .then((result) => {
+          if (result.type == 'business') { 
+            this.router.navigate(['/business-profile', this.user._id])
+          } else if (result.type =='user'){
+            this.showAlert = false
+          } else {
+            this.router.navigate(['/login'])
+          }
+        // ... navigate with this.router.navigate(['...'])
+        })
+        .catch((err) => {
+          this.processing = false;
+          this.error =  err.error.error;
+          this.feedbackEnabled = false;
+        });
+  }
   
   goBack() {
     this.location.back();
